94eae59aec60e44731f1186cb1c6b60051d213af
uu <2543954706@qq.com>
2022-05-21 22:47:25 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
     entry: './src/index.js',
     output: {
-    filename: '[name].[contenthash].js',
-  },
+    filename: 'index.[contenthash].js',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'uu77',
+        template: 'src/assets/index.html'
+      }),
+      new MiniCssExtractPlugin()
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+      },
+    module: {
+    rules: [
+        {
+        test: /\.css$/i, 
+        // use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+    ],
+    },
     mode: 'development'
 };
